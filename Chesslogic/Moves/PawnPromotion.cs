62c6b41ea4ca2649b702190ac72988989c6fbd18
Joseph Doan <josephtpdoan@gmail.com>
2024-10-19 20:13:55 -0400

    pawn promotion - ui added, logic added

---
+++ Chesslogic/Moves/PawnPromotion.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chesslogic
+{
+    public class PawnPromotion: Move
+    {
+        public override MoveType Type => MoveType.PawnPromotion;
+        public override Position FromPos { get; }
+        public override Position ToPos { get; }
+
+        public readonly PieceType newType;
+
+        public PawnPromotion(Position form, Position to, PieceType newType)
+        {
+            FromPos = form;
+            ToPos = to;
+            this.newType = newType;
+        }
+
+        private Piece CreatePromotionPiece(Player color)
+        {
+            return newType switch
+            {
+                PieceType.Knight => new Knight(color),
+                PieceType.Bishop => new Bishop(color),
+                PieceType.Rook => new Rook(color),
+                _ => new Queen(color)
+            };
+        }
+
+        public override void Execute(Board board)
+        {
+            Piece pawn = board[FromPos];
+            board[FromPos] = null;
+
+            Piece promotionPiece = CreatePromotionPiece(pawn.Color);
+            promotionPiece.HasMoved = true;
+            board[ToPos] = promotionPiece;
+        }
+    }
+}
